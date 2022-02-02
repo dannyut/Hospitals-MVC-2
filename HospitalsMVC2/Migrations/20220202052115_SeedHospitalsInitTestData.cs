c14025d3e47f0496c960d26f4d37acd5a4629aa7
dannyut <dannyv40@gmail.com>
2022-02-01 23:23:09 -0600

    migration to seed init hospitals test data

---
+++ HospitalsMVC2/Migrations/20220202052115_SeedHospitalsInitTestData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalsMVC2.Migrations
+{
+    public partial class SeedHospitalsInitTestData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hospitals",
+                columns: new[] { "HospitalId", "Address", "Email", "Name", "Phone" },
+                values: new object[] { 1, "Dallas, TX", "UTSMC@test.com", "UT Southwestern Medical Center", "(123) 123-4567" });
+
+            migrationBuilder.InsertData(
+                table: "Hospitals",
+                columns: new[] { "HospitalId", "Address", "Email", "Name", "Phone" },
+                values: new object[] { 2, "Dallas, TX", "BUMC@test.com", "Baylor University Medical Center", "(111) 111-1111" });
+
+            migrationBuilder.InsertData(
+                table: "Hospitals",
+                columns: new[] { "HospitalId", "Address", "Email", "Name", "Phone" },
+                values: new object[] { 3, "Dallas, TX", "MCD@test.com", "Medical City Dallas", "(222) 222-2222" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hospitals",
+                keyColumn: "HospitalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hospitals",
+                keyColumn: "HospitalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hospitals",
+                keyColumn: "HospitalId",
+                keyValue: 3);
+        }
+    }
+}
--- HospitalsMVC2/Models/AppDbContext.cs
+++ HospitalsMVC2/Models/AppDbContext.cs
@@ -11,6 +11,38 @@ namespace HospitalsMVC2.Models
 
         public DbSet<Hospital> Hospitals { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed data
+            modelBuilder.Entity<Hospital>().HasData(new Hospital
+            {
+                HospitalId = 1,
+                Name = "UT Southwestern Medical Center",
+                Address = "Dallas, TX",
+                Email = "UTSMC@test.com",
+                Phone = "(123) 123-4567"
+            });
+            modelBuilder.Entity<Hospital>().HasData(new Hospital
+            {
+                HospitalId = 2,
+                Name = "Baylor University Medical Center",
+                Address = "Dallas, TX",
+                Email = "BUMC@test.com",
+                Phone = "(111) 111-1111"
+            });
+            modelBuilder.Entity<Hospital>().HasData(new Hospital
+            {
+                HospitalId = 3,
+                Name = "Medical City Dallas",
+                Address = "Dallas, TX",
+                Email = "MCD@test.com",
+                Phone = "(222) 222-2222"
+            });
+
+        }
+
     }
 
 }
